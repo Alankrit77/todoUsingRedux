a94196de51c43502fe8214ee70e5658215437848
Alankrit Traipthi <92813104+Alankrit77@users.noreply.github.com>
2024-04-21 18:19:26 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import AddTodo from "./Components/AddTodo";
+import Todo from "./Components/Todos";
+
+function App() {
+  return (
+    <>
+      <h1>learn about redux</h1>
+      <AddTodo />
+      <Todo />
+    </>
+  );
+}
+
+export default App;
